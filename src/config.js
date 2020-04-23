cbbe2066153a478c41a189315e4f0cd0a3cdc8ac
System Administrator <root@Twishishs-MacBook-Air.local>
2020-04-23 15:15:16 -0400

    Setting up our React App

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+  s3: {
+    REGION: "us-east-1",
+    BUCKET: "notestaking-app-uploads"
+  },
+  apiGateway: {
+    REGION: "us-east-1",
+    URL: "https://pvqfvloxek.execute-api.us-east-1.amazonaws.com/prod"
+  },
+  cognito: {
+    REGION: "us-east-1",
+    USER_POOL_ID: "us-east-1_d5Tqm1wMT",
+    APP_CLIENT_ID: "5jb6ckur3am82a4k9elfdgi70m",
+    IDENTITY_POOL_ID: "us-east-1:db92d94b-3e5d-4c9b-9948-d81a7ef40b98"
+  }
+};
